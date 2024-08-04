9fd071abda41d8a730ca78631586ae0f814ee065
Sr <sr@gmail.com>
2024-08-04 12:05:46 +0530

    added

--- src/test/java/myPacakge/LaunchBrowser.java
+++ src/test/java/myPacakge/LaunchBrowser.java
@@ -6,24 +6,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchBrowser {
 
+
     static WebDriver driver;
 
     public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
          driver = new ChromeDriver();
+         driver.get("https://www.google.com/");
 
       //  driver.get("https://demo.nopcommerce.com/");
        // driver.get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
       //  driver.get("https://www.opencart.com/index.php?route=account/register");
-        driver.get("https://www.toolsqa.com/selenium-training?q=banner#enroll-form");
+       // driver.get("https://www.toolsqa.com/selenium-training?q=banner#enroll-form");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
        // WebElementStatus.elemenStatus();
         //Navigations.navigations();
        // FindElementVsElements.findElemenElements();
        // GetText_GetAttribute.getTextAttribute();
-        selectDropDown.getDropdown();
+       // selectDropDown.getDropdown();
 
         driver.quit();
         driver.quit();
